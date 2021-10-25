dbd2b8e19ff5f822bda686ad8bc90999a0ca09e3
Maurizio-P <mauriziopignotti@gmail.com>
2021-10-25 14:58:45 +0200

    pause snack1

--- snack1/js/script.js
+++ snack1/js/script.js
@@ -1,1 +1,41 @@
+"use strict"
+
 console.log("start")
+
+const arrayBici = [
+    {
+        name: "triban",
+        weight: "18"
+    },
+    {
+        name: "btwin",
+        weight: "16"
+    },
+    {
+        name: "gravel",
+        weight: "22"
+    },
+    {
+        name: "gravelXL",
+        weight: "24"
+    }
+]
+
+console.log(arrayBici);
+
+const copyArrayBici = [...arrayBici]
+console.log(copyArrayBici);
+
+forArrayBici(copyArrayBici)
+
+function forArrayBici(array){
+    for (let i = 0; i < array.length; i++) {
+        const {name, weight} = array[i];
+        console.log({name, weight});
+    }
+
+}
+
+
+
+
